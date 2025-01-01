2d31653e515f505531c1a03f75b4477c3669be9e
Anh Tu <sweantu@gmail.com>
2025-01-01 18:31:16 +0700

    feat: cors admin page

--- app/core/config.py
+++ app/core/config.py
@@ -22,8 +22,10 @@ class Settings:
 
     CORS_ORIGINS: list = [
         "http://localhost:5173",
+        "http://localhost:5174",
         "http://localhost:8000",
         "http://127.0.0.1:5173",
+        "http://127.0.0.1:5174",
         "http://127.0.0.1:8000",
     ]
 
